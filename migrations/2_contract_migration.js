9dc9d2b72df5126394c4d275dacd0549e9df8cb6
usagi.f@gmail.com <usagi.f@gmail.com>
2018-04-12 19:03:41 +0900

    add: cryptozombies L1

--- migrations/2_contract_migration.js
+++ migrations/2_contract_migration.js
@@ -1,5 +1,7 @@
 const HelloWorld = artifacts.require("HelloWorld");
+const ZombieFactory = artifacts.require("ZombieFactory");
 
 module.exports = (deployer) => {
   deployer.deploy(HelloWorld);
+  deployer.deploy(ZombieFactory);
 };
